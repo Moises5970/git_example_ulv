dc75c19302c48688b789e7a3689964a28edb488e
José Moisés Martínez Hernández <jose.martinez@ulv.edu.mx>
2024-11-18 08:28:53 -0600

    iniciar

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Bienvenido al programa de saludos");
+
+function saludar (nombre, edad){
+    return `Hola ${nombre}! como estas?`
+}
+
+const nombreUsuario = "Estudiante";
+console.log(saludar (nombreUsuario));
+
